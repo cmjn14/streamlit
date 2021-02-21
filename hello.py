6d7f39a2f116c7ac1aef399906390192c89511dc
cmjn14 <cedric.lopez@free.fr>
2021-02-21 18:14:55 +0100

    initialization

---
+++ hello.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+
+Note - This can be run from anaconda prompt using -  streamlit run "hello.py"
+
+"""
+
+
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+st.title('My first app')
+
+st.write("Here's our first attempt at using data to create a table:")
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
