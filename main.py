2c7e2936402a8c9cdf24e688d8b1e2fd242f9a68
cmjn14 <cedric.lopez@free.fr>
2022-05-02 11:39:02 +0200

    fix

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def request_works(concept_name):
     if len(concept_name) != 0:
         search_works = requests.get('https://api.openalex.org/works?search=' + concept_name.replace(" ","%20")).json()['results']
         for work in search_works:
-            st.write(result['display_name'])
+            st.write(work['display_name'])
     return True
 
 searched_concept = st.text_input("Search concepts:", value="")
