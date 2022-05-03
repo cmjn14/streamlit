02618e17dc810163901bbb57638e480c0fab7224
cmjn14 <cedric.lopez@free.fr>
2022-05-03 15:10:20 +0200

    fix

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def request_works(concept_name):
                 else:
                     authors_list.append(author_display_name)
             st.markdown(", ".join(authors_list))
-            st.markdown(urllib.quote(work['doi']))
+            st.markdown(urllib.parse.quote(work['doi']))
             st.markdown('<a href="' + work['doi'] + '">' + work['doi'] + '</a>', unsafe_allow_html=True)
             st.caption(work['open_access']['is_oa'])
             st.caption(str(work['cited_by_count']) + " citations")        
