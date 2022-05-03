ccef32129a3436d00022d56fe811e910440cea04
cmjn14 <cedric.lopez@free.fr>
2022-05-03 15:14:10 +0200

    safe

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def request_works(concept_name):
                     authors_list.append(author_display_name)
             st.markdown(", ".join(authors_list))
             st.markdown(urllib.parse.quote(work['doi']))
-            st.markdown('<a href="' + urllib.parse.quote(work['doi']) + '">' + work['doi'] + '</a>')
+            st.markdown('<a href="' + urllib.parse.quote(work['doi'], safe=':/') + '">' + work['doi'] + '</a>')
             st.caption(work['open_access']['is_oa'])
             st.caption(str(work['cited_by_count']) + " citations")        
     return True
