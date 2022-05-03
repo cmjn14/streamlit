a387f5f25c191ed21da0684f1d2cc9e9a87faab6
cmjn14 <cedric.lopez@free.fr>
2022-05-03 15:16:03 +0200

    dvf

--- main.py
+++ main.py
@@ -73,7 +73,6 @@ def request_works(concept_name):
                     authors_list.append(author_display_name)
             st.markdown(", ".join(authors_list))
             st.markdown(urllib.parse.quote(work['doi'], safe=':/'))
-            st.markdown('<a href="' + urllib.parse.quote(work['doi'], safe=':/') + '">' + work['doi'] + '</a>')
             st.caption(work['open_access']['is_oa'])
             st.caption(str(work['cited_by_count']) + " citations")        
     return True
