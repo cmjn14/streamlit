629d72348a94229cd25762185a0bd2d162de1915
cmjn14 <cedric.lopez@free.fr>
2022-05-04 15:32:15 +0200

    jhgjhg

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def request_concepts(searchedterm):
     if len(searchedterm) != 0:
         searchresults = requests.get('https://api.openalex.org/autocomplete/concepts?q=' + searchedterm + polite).json()['results']
         for result in searchresults:
-            st.info(f"{result['display_name']}<br/><small>{result['hint']}</small>", allow_unsafe_html=True)
+            st.markdown(f"{result['display_name']}<br/><small>{result['hint']}</small>", allow_unsafe_html=True)
     return True
 
 def request_works(concept_name):
